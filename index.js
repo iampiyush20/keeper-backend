5aaf8c6509d8b88fafd310d6171902d859249256
piyush <piyushvijay20002@gmail.com>
2022-09-12 19:30:13 +0530

    first

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const cors=require('cors');
 require('./Db/Config');
 const app=express();
+app.use(cors());
 app.use(express.json());
 // const PORT=process.env.PORT || 5000;
 const Task= require('./Db/Task');
